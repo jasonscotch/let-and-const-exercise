eedaedf93fa737f198f29cc90cc2c7a7810fb45b
jasonscotch <jason.m.scot@gmail.com>
2022-12-21 21:40:55 -0800

    uploading let and const exercise

---
+++ app.js
@@ -0,0 +1,20 @@
+// var PI = 3.14;
+// PI = 42;
+
+const PI = 3.14;
+PI = 42; //Gives an error.
+
+
+
+
+//What is the difference between var and let?
+// With var you can: reassign, redeclare, and mutate the variable. With let, you can reassign and mutate the variable but not redeclare it. 
+
+// What is the difference between var and const?
+// With const, you cannot reassign or redeclare the variable but you can mutate it. 
+
+// What is the difference between let and const?
+//With let, you can reassign and mutate the variable but not redeclare it. With const, you cannot reassign or redeclare the variable but you can mutate it. 
+
+// What is hoisting?
+//In JavaScript, Hoisting is the default behavior of moving all the declarations at the top of the scope before code execution. Basically, it gives us an advantage that no matter where functions and variables are declared, they are moved to the top of their scope regardless of whether their scope is global or local. It allows us to call functions before even writing them in our code. 
